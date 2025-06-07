9cab50136846823d5ba24de21d7f5afd47aea9b8
alireza <rexalirz72@gmail.com>
2025-06-07 19:24:31 +0330

    add useRawInitData

--- src/components/layout/MainLayout.tsx/index.tsx
+++ src/components/layout/MainLayout.tsx/index.tsx
@@ -1,7 +1,7 @@
 import { Outlet, useNavigate } from 'react-router'
 import BottomNavigation from '../BottomNavigation'
 import { useEffect } from 'react';
-import { useLaunchParams } from '@telegram-apps/sdk-react';
+import { useLaunchParams, useRawInitData, useRawLaunchParams } from '@telegram-apps/sdk-react';
 
 export default function MainLayout() {
     const params = useLaunchParams()
@@ -13,7 +13,9 @@ export default function MainLayout() {
         console.log("1111111",startParam);
         console.log("222222222",params);
         console.log("333333",params.tgWebAppData?.start_param);
-        
+        console.log(useRawLaunchParams() );
+                console.log(useRawInitData());
+                  
 
         if (startParam) {
             navigate("/product/" + startParam)
