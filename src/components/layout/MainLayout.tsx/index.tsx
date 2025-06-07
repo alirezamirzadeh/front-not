4c9f3d513316393265bd8da953059b0db9f236b9
alireza <rexalirz72@gmail.com>
2025-06-07 18:44:54 +0330

    test useLaunchParams

--- src/components/layout/MainLayout.tsx/index.tsx
+++ src/components/layout/MainLayout.tsx/index.tsx
@@ -1,13 +1,20 @@
 import { Outlet, useNavigate } from 'react-router'
 import BottomNavigation from '../BottomNavigation'
 import { useEffect } from 'react';
+import { useLaunchParams } from '@telegram-apps/sdk-react';
 
 export default function MainLayout() {
-    const params = new URLSearchParams(window.location.search);
-    const startParam = params.get('product');
+    const params = useLaunchParams()
+    const startParam = params.tgWebAppStartParam
     const navigate = useNavigate()
 
+
     useEffect(() => {
+        console.log("1111111",startParam);
+        console.log("222222222",params);
+        console.log("333333",params.tgWebAppData?.start_param);
+        
+
         if (startParam) {
             navigate("/product/" + startParam)
         }
