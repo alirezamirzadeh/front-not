65ba038b4cf74b84b6ecf1401fc0fcd02541ac87
alireza <rexalirz72@gmail.com>
2025-06-12 07:20:36 +0330

    fix select image

--- src/components/ui/ProductImage/index.tsx
+++ src/components/ui/ProductImage/index.tsx
@@ -28,7 +28,7 @@ export const ProductImage = memo(({ product }: { product: Product }) => {
 
     const { selectedIndex, setSelectedImageIndex } = useProductsStore(
         useShallow(s => ({
-            selectedIndex: s.selectedImageIndices[product.id] ?? 0,
+            selectedIndex: s.selectedImageIndices[product.id] ?? product.id -1,
             setSelectedImageIndex: s.setSelectedImageIndex,
         }))
     );
